b15a7bdbeeccb2bee4e06b698d7617710c929640
Attari Ak <akaabdullah00@gmail.com>
2021-12-06 00:44:23 +0500

    newpage added

--- pages/customcutprintreport.js
+++ pages/customcutprintreport.js
@@ -2,7 +2,6 @@
 export const getStaticProps = async () => {
     const res = await fetch('https://nextfabglass.vercel.app/api/orders');
     const { data } = await res.json();
-    console.log(data);
     const count = Object.keys(data).length;
     console.log(count);
     const repeated = data.reduce((acc, cur) => {
@@ -25,7 +24,6 @@ export const getStaticProps = async () => {
     var len = result.length;
     console.log(len);
 
-
     return {
         props: { reports: data, count: count, result: result, len: len },
         revalidate: 300,
@@ -38,7 +36,10 @@ export const getStaticProps = async () => {
 
 
 import { useState } from 'react';
+import router, { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
+
+//Getting date wise data from database
 
 
 const Customcutprintreport = ({ reports, count, result, len }) => {
@@ -51,6 +52,15 @@ const Customcutprintreport = ({ reports, count, result, len }) => {
     const day = myArray[2];
     const finalDate = day + '/' + month + '/' + year;
     console.log(finalDate);
+    if (typeof window !== "undefined") {
+
+        localStorage.setItem(date, finalDate);
+
+    }
+    function getDatewiseData() {
+        router.push('/datewisereport');
+    }
+
 
     const [doubleshow, SetDoubleShow] = useState(false);
     const HandleDoublePrint = () => {
@@ -72,26 +82,27 @@ const Customcutprintreport = ({ reports, count, result, len }) => {
                     <div className="column is-3">
                         <div className="box has-background-dark">
                             <p className="subtitle has-text-centered has-text-white">Search Order No</p>
-                            <input className="input is-small mb-3" type="search" onChange={event => { SetSearchOrder(event.target.value) }} placeholder="Search...."></input>
+                            <input className="input is-small mb-5" type="search" onChange={event => { SetSearchOrder(event.target.value) }} placeholder="Search...."></input>
                         </div>
                     </div>
                     <div className="column is-3">
                         <div className="box has-background-dark">
-                            <p className="subtitle has-text-centered has-text-white">Select Date</p>
-                            <input className="input" onChange={event => { SetDate(event.target.value) }} type="date"></input>
+                            <p className="subtitle has-text-centered has-text-white mb-1">Select Date</p>
+                            <input className="input mb-1" onChange={event => { SetDate(event.target.value) }} type="date"></input>
+                            <button onClick={getDatewiseData} className="button is-link is-small is-fullwidth">Search</button>
                         </div>
                     </div>
                     <div className="column is-3">
                         <div className="box has-background-warning-dark	">
                             <p className="subtitle has-text-centered has-text-white">Total printed Orders</p>
-                            <p className="subtitle has-text-centered has-text-white mb-4"> <strong className="has-text-white"> {count}</strong></p>
+                            <p className="subtitle has-text-centered has-text-white mb-5"> <strong className="has-text-white"> {count}</strong></p>
                         </div>
                     </div>
                     <div className="column is-3">
                         <div className="box has-background-dark">
-                            <p className="subtitle has-text-centered has-text-white m-1">Double Printed Orders</p>
+                            <p className="subtitle has-text-centered has-text-white ">Double Printed Orders</p>
                             <p className="subtitle has-text-centered has-text-white m-1">{len}</p>
-                            <a className="has-text-centered button is-small is-rounded is-success is-fullwidth" onClick={HandleDoublePrint}> View Details</a>
+                            <a className="has-text-centered button is-small is-link is-fullwidth" onClick={HandleDoublePrint}> View Details</a>
                         </div>
                     </div>
                 </div>
@@ -99,19 +110,19 @@ const Customcutprintreport = ({ reports, count, result, len }) => {
                     !doubleshow ?
                         <div className={styles.tablewrapper}>
                             <div className="columns is-centered" >
-                                <table className="table has-text-centered">
-                                    <thead className="has-text-centered">
-                                        <tr className="has-text-centered is-selected">
-                                            <th className="has-text-black">ID</th>
-                                            <th className="has-text-black">Order Number</th>
-                                            <th className="has-text-black">Time</th>
-                                            <th className="has-text-black">Status</th>
-                                            <th className="has-text-black">Printer</th>
-                                            <th className="has-text-black">Copies</th>
-                                            <th className="has-text-black">Size</th>
-                                            <th className="has-text-black">Page Height</th>
-                                            <th className="has-text-black">Page Width</th>
-                                            <th className="has-text-black">Duplex</th>
+                                <table className="has-background-white-ter table has-text-centered">
+                                    <thead className=" has-text-centered">
+                                        <tr className=" has-background-dark has-text-centered is-selected">
+                                            <th className="has-text-white has">ID</th>
+                                            <th className="has-text-white has">Order Number</th>
+                                            <th className="has-text-white has">Time</th>
+                                            <th className="has-text-white has">Status</th>
+                                            <th className="has-text-white has">Printer</th>
+                                            <th className="has-text-white has">Copies</th>
+                                            <th className="has-text-white has">Size</th>
+                                            <th className="has-text-white has">Page Height</th>
+                                            <th className="has-text-white has">Page Width</th>
+                                            <th className="has-text-white has">Duplex</th>
                                         </tr>
                                     </thead>
                                     {reports.filter((val) => {
---
+++ pages/datewisereport.js
@@ -0,0 +1,11 @@
+const datewisereport = () => {
+    return (
+        <div className="container">
+            <p className="title has-text-centered">
+                Date Wise Report
+            </p>
+        </div>
+    )
+}
+
+export default datewisereport
